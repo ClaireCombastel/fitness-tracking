1ec26992cf2e55af308104eb22477dd133062e8c
ClaireCombastel <combastel.cl@gmail.com>
2021-03-11 08:27:17 +0100

    Post Model

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -6,12 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  //posts = [
-   // { title: 'First Post', content: 'This is the first post\'s content' },
-   // { title: 'Second Post', content: 'This is the second post\'s content' },
-   // { title: 'Third Post', content: 'This is the third post\'s content' },
- // ];
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
   constructor() { }
 
   ngOnInit(): void {
